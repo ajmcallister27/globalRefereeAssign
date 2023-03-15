621030881658537ec46e94df162f5e828a467993
Andrew McAllister <ajmcallister27@gmail.com>
2023-03-15 12:11:22 -0700

    Fixed login issues

--- pageScraper.js
+++ pageScraper.js
@@ -12,41 +12,39 @@ const scraperObject = {
 
 			let pagePromise = (link) => new Promise(async (resolve, reject) => {
 				let dataObj = {};
-				let newPage = await browser.newPage();
-				await newPage.goto(link);
-                await newPage.type('#sitename', 'Andrew James McAllister');
-                await newPage.type('#password', 'WyceQSA59h!jJbk');
+				let page = await browser.newPage();
+				await page.goto(link);
+                await page.type('#sitename', 'Andrew James McAllister');
+                await page.type('#password', 'WyceQSA59h!jJbk');
                 await page.click('#zLogon');
 
-                await page.waitForNavigation();
-
-				// if (await newPage.$eval('#col2 > dd > h1', text => text.textContent) !== 'Search AskTheRef.com Q&A Database') {
-				// 	dataObj['questionTitle'] = await newPage.$eval('#col2 > dd > h1', text => text.textContent);
-				// 	dataObj['questionSubtitle'] = await newPage.$eval('#col2 > dd > h2', text => text.textContent);
-				// 	if (await newPage.$('#col2 > dd > h3 > strong') || "") dataObj['questionAsker'] = await newPage.$eval('#col2 > dd > h3 > strong', text => text.textContent);
-				// 	if (await newPage.$('#col2 > dd > p > strong') || "") dataObj['questionLevel'] = await newPage.$eval('#col2 > dd > p > strong', text => text.textContent);
-				// 	dataObj['question'] = await newPage.$eval('#col2 > dd > p:last-child', text => text.textContent);
+				// if (await page.$eval('#col2 > dd > h1', text => text.textContent) !== 'Search AskTheRef.com Q&A Database') {
+				// 	dataObj['questionTitle'] = await page.$eval('#col2 > dd > h1', text => text.textContent);
+				// 	dataObj['questionSubtitle'] = await page.$eval('#col2 > dd > h2', text => text.textContent);
+				// 	if (await page.$('#col2 > dd > h3 > strong') || "") dataObj['questionAsker'] = await page.$eval('#col2 > dd > h3 > strong', text => text.textContent);
+				// 	if (await page.$('#col2 > dd > p > strong') || "") dataObj['questionLevel'] = await page.$eval('#col2 > dd > p > strong', text => text.textContent);
+				// 	dataObj['question'] = await page.$eval('#col2 > dd > p:last-child', text => text.textContent);
 
 				// 	dataObj['responses'] = [];
-				// 	responses = await newPage.$$('#col2 > dd');
+				// 	responses = await page.$$('#col2 > dd');
 
 				// 	for (let i = 2; i <= responses.length - 2; i++) {
 				// 		let responseObj = {};
-				// 		if (await newPage.$(`#col2 > dd:nth-child(${i}) > h4`) || "") {
-				// 			responseObj.responder = await newPage.$eval(`#col2 > dd:nth-child(${i}) > h4`, text => text.textContent);
-				// 			responseObj.answer = await newPage.$eval(`#col2 > dd:nth-child(${i}) > p`, text => text.textContent);
+				// 		if (await page.$(`#col2 > dd:nth-child(${i}) > h4`) || "") {
+				// 			responseObj.responder = await page.$eval(`#col2 > dd:nth-child(${i}) > h4`, text => text.textContent);
+				// 			responseObj.answer = await page.$eval(`#col2 > dd:nth-child(${i}) > p`, text => text.textContent);
 				// 			dataObj['responses'].push(responseObj);
 				// 		}
 				// 	}
 				// }
 				// resolve(dataObj);
-				await newPage.close();
+				await page.close();
 			});
 
 			// for (let linkNum = 12; linkNum <= 20; linkNum++) {
-			// 	let currentPageData = await pagePromise(`http://asktheref.com/search.asp?QuestionID=${linkNum}`);
+				let currentPageData = await pagePromise(`https://www.ekcsra.org/logon`);
 			// 	console.log(`Scraped started on question ${linkNum}`)
-			// 	scrapedData.push(currentPageData);
+				scrapedData.push(currentPageData);
 			// }
 
 			return scrapedData;
