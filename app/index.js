2a9a9e801d5886f55994b9b58505e950a7ceafc2
Andrew McAllister <mcallisteraj@students.svsd410.org>
2023-04-01 15:53:45 -0700

    Added a lot of features

--- app/index.js
+++ app/index.js
@@ -1,7 +1,45 @@
 // What to do:
-//  - Get prices from SnVYSA
-//  - Get prices from EKCSRA
-//  - Get prices from NSO
-//  - Display all prices
+//  - Get games from SnVYSA
+//  - Get games from EKCSRA
+//  - Get games from NSO
+//  - Display all games
 //  - Update
 
+const fs = require('fs');
+const allEkcsraGames = JSON.parse(fs.readFileSync('data/ekcsraGames.json', 'utf-8'));
+const allGameOfficialsGames = JSON.parse(fs.readFileSync('data/gameOfficialsGames.json', 'utf-8'));
+const allNSOGames = JSON.parse(fs.readFileSync('data/NSOGames.json', 'utf-8'));
+
+let allGames = [];
+
+function combineSources() {
+    allGames = allEkcsraGames.concat(allGameOfficialsGames).concat(allNSOGames);
+    fs.writeFile('data/allGames.json', JSON.stringify(allGames), (error) => {
+        if (error) console.log(error);
+        console.log('Data written to allGames.json successfully')
+    });
+}
+
+function filterGames(filter) {
+    if (filter === 'available') {
+        let availableGames = [];
+        
+        for (let i = 0; i < allEkcsraGames.length; i++) {
+            let game = allEkcsraGames[i];
+            if (game.ref == ' ' || game.ar1 == '' || game.ar2 == '' || game.r4 == '') {
+                availableGames.push(game);
+            }
+        }
+		fs.writeFile('data/availableGames.json', JSON.stringify(availableGames), (error) => {
+			if (error) console.log(error);
+			console.log('Data written to availableGames.json successfully')
+		});
+    }
+}
+
+function init() {
+    combineSources();
+    filterGames('available');
+}
+
+init();
--- ekcsra/pageScraper.js
+++ ekcsra/pageScraper.js
@@ -9,7 +9,6 @@ const scraperObject = {
 		await page.type('#sitename', 'Andrew James McAllister');
 		await page.type('#password', 'WyceQSA59h!jJbk');
 		await page.click('#zLogon');
-		await page.waitForNavigation();
 		await page.goto('https://www.ekcsra.org/refereeinquiry');
 		let scrapedData = [];
 
@@ -24,34 +23,102 @@ const scraperObject = {
 				links.push(url);
 			}
 
-			let pagePromise = (link) => new Promise(async (resolve, reject) => {
+			let pagePromise = (link, gameId) => new Promise(async (resolve, reject) => {
 				let dataObj = {};
-				let date = '';
+				let date, time, field, gender, league, client, level, authority, division, season, priority, type, status, rank, home, away, ref, ar1, ar2, r4, mtr, refPay, ar1Pay, ar2Pay, r4Pay, mtrPay;
 				let infoPage = await browser.newPage();
-				infoPage.goto(link);
+				await infoPage.goto(link);
 
-				console.log(`I went to ${link}`)
 				date = await infoPage.$eval(`tr:nth-child(2) > .inputNormal:nth-child(2)`, text => text.textContent);
-				console.log('I work')
-				console.log(date);
+				time = await infoPage.$eval(`tr:nth-child(2) > .inputNormal:nth-child(4)`, text => text.textContent);
+				field = await infoPage.$eval(`tr:nth-child(3) > .inputNormal:nth-child(2)`, text => text.textContent);
+				gender = await infoPage.$eval(`tr:nth-child(3) > .inputNormal:nth-child(4)`, text => text.textContent);
+				league = await infoPage.$eval(`tr:nth-child(4) > .inputNormal:nth-child(2)`, text => text.textContent);
+				client = await infoPage.$eval(`tr:nth-child(4) > .inputNormal:nth-child(4)`, text => text.textContent);
+				level = await infoPage.$eval(`tr:nth-child(5) > .inputNormal:nth-child(2)`, text => text.textContent);
+				authority = await infoPage.$eval(`tr:nth-child(5) > .inputNormal:nth-child(4)`, text => text.textContent);
+				division = await infoPage.$eval(`tr:nth-child(6) > .inputNormal:nth-child(2)`, text => text.textContent);
+				season = await infoPage.$eval(`tr:nth-child(6) > .inputNormal:nth-child(4)`, text => text.textContent);
+				priority = await infoPage.$eval(`tr:nth-child(7) > .inputNormal:nth-child(2)`, text => text.textContent);
+				type = await infoPage.$eval(`tr:nth-child(7) > .inputNormal:nth-child(4)`, text => text.textContent);
+				status = await infoPage.$eval(`tr:nth-child(8) > .inputNormal:nth-child(2)`, text => text.textContent);
+				rank = await infoPage.$eval(`tr:nth-child(8) > .inputNormal:nth-child(4)`, text => text.textContent);
+				home = await infoPage.$eval(`tr:nth-child(9) > .inputNormal:nth-child(2)`, text => text.textContent);
+				away = await infoPage.$eval(`tr:nth-child(9) > .inputNormal:nth-child(4)`, text => text.textContent);
+				ref = await infoPage.$eval(`tr:nth-child(11) > .input`, text => text.textContent);
+				try { ar1 = await infoPage.$eval(`tr:nth-child(12) > .input`, text => text.textContent) } catch(err) { ar1 = "nan" };
+				try { ar2 = await infoPage.$eval(`tr:nth-child(13) > .input`, text => text.textContent) } catch(err) { ar2 = "nan" };
+				try { r4 = await infoPage.$eval(`tr:nth-child(14) > .input`, text => text.textContent) } catch(err) { r4 = "nan" };
+			 	try { mtr = await infoPage.$eval(`tr:nth-child(15) > .input`, text => text.textContent) } catch(err) { mtr = "nan" };
+				refPay = await infoPage.$eval(`tr:nth-child(11) > .inputNormal`, text => text.textContent);
+				try { ar1Pay = await infoPage.$eval(`tr:nth-child(12) > .inputNormal`, text => text.textContent) }  catch(err) { ar1Pay = "nan" };
+				try { ar2Pay = await infoPage.$eval(`tr:nth-child(13) > .inputNormal`, text => text.textContent) }  catch(err) { ar2Pay = "nan" };
+				try { r4Pay = await infoPage.$eval(`tr:nth-child(14) > .inputNormal`, text => text.textContent) }  catch(err) { r4Pay = "nan" };
+				try { mtrPay = await infoPage.$eval(`tr:nth-child(15) > .inputNormal`, text => text.textContent) }  catch(err) { mtrPay = "nan" };
+
+				dataObj = {
+					source: 'EKCSRA',
+					id: gameId,
+					date: date,
+					time: time,
+					field: field,
+					gender: gender,
+					league: league,
+					client: client,
+					level: level,
+					authority: authority,
+					division: division,
+					season: season,
+					priority: priority,
+					type: type,
+					status: status,
+					rank: rank,
+					home: home,
+					away: away,
+					ref: ref,
+					ar1: ar1,
+					ar2: ar2,
+					r4: r4,
+					mtr: mtr,
+					refPay: refPay,
+					ar1Pay: ar1Pay,
+					ar2Pay: ar2Pay,
+					r4Pay: r4Pay,
+					mtrPay: mtrPay,
+				}
 				resolve(dataObj);
+				await infoPage.close();
 			});
 
 			for (let linkNum = 0; linkNum < links.length; linkNum++) {
-				console.log(links);
 				console.log(`on link ${links[linkNum]}`)
-				let currentPageData = await pagePromise(`https://www.ekcsra.org/refereeinquiry?action=Display&key=${links[linkNum]}`);
+				let currentPageData = await pagePromise(`https://www.ekcsra.org/refereeinquiry?action=Display&key=${links[linkNum]}`, links[linkNum]);
 				scrapedData.push(currentPageData);
 			}
 
+			// Pagination control
+			let nextButtonExist = false;
+			try {
+				const nextButton = await page.$eval('.next > a', a => a.textContent);
+				nextButtonExist = true;
+			}
+			catch(err) {
+				nextButtonExist = false;
+			}
+			if (nextButtonExist) {
+				await page.click('.next > a');
+				return scrapeCurrentPage();
+			}
+			await page.close();
+
 			return scrapedData;
 		}
+
 		let data = await scrapeCurrentPage();
-		console.log(data);
-		// fs.writeFile('games.json', JSON.stringify(data), (error) => {
-		// 	if (error) console.log(error);
-		// 	console.log('Data written successfully')
-		// });
+		fs.writeFile('data/ekcsraGames.json', JSON.stringify(data), (error) => {
+			if (error) console.log(error);
+			console.log('Data written to ekcsraGames.json successfully')
+		});
 		return data;
 	}
 }
